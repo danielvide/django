7382b30c369e432cc0b8ef6b2e8a540fa4bc3968
danielvide <dani.villa271@hotmail.com>
2019-05-10 21:25:55 +0200

    validacion formulario

--- src1/boletin/views.py
+++ src1/boletin/views.py
@@ -6,7 +6,11 @@ from .forms import RegForm
 
 # Create your views here.
 def inicio(request):
-	form = RegForm()
+	form = RegForm(request.POST or None)
+	if form.is_valid():
+		form_data=form.cleaned_data
+		print form_data.get("nombre")
+		print form_data.get("edad")
 	context= {
 		"form": form,
 	}
