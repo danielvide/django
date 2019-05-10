778a8b4c44a5ff86893f3db2825aae9d6f46a101
danielvide <dani.villa271@hotmail.com>
2019-05-10 20:48:30 +0200

    creacion de formulario

--- src1/boletin/views.py
+++ src1/boletin/views.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .forms import RegForm
 
 # Create your views here.
 def inicio(request):
-	return render(request,"inicio.html",{})
+	form = RegForm()
+	context= {
+		"form": form,
+	}
+	return render(request,"inicio.html",context)
