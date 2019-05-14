50d091061ff1aedfa4dd20d95759a2a9eba18cfb
danielvide <dani.villa271@hotmail.com>
2019-05-14 21:18:39 +0200

    Validaciones

--- src1/boletin/forms.py
+++ src1/boletin/forms.py
@@ -6,6 +6,18 @@ class RegModelForm(forms.ModelForm):
 		model=Registrado
 		fields=["email","nombre"]
 
+	def clean_email(self):
+		email=self.cleaned_data.get("email")
+		email_base,proveeder=email.split("@")
+		dominio,extension=proveeder.split(".")
+		if not extension == "edu":
+			raise forms.ValidationError("Por favor utiliza un email con extension .EDU")
+		return email
+	def clean_nombre(self):
+		nombre=self.cleaned_data.get("nombre")
+		#validaciones
+		return nombre
+
 class RegForm(forms.Form):
 	nombre= forms.CharField(max_length=100)
 	email=forms.EmailField()
