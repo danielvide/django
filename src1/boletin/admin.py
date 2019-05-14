770569c6c17622bb843c925a406810311cc5196f
danielvide <dani.villa271@hotmail.com>
2019-05-14 21:04:55 +0200

    Model Form

--- src1/boletin/admin.py
+++ src1/boletin/admin.py
@@ -2,16 +2,17 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .forms import RegModelForm
 # Register your models here.
 from .models import Registrado
 
 class AdminRegistrado(admin.ModelAdmin):
 	list_display = ["__unicode__","nombre","timestamp"]
+	form = RegModelForm
 	list_filter = ["timestamp"]
 	list_editable = ["nombre"]
 	search_fields = ["email","nombre"]
-	class Meta:
-		model= Registrado
+	#class Meta:
+	#	model= Registrado
 
 admin.site.register(Registrado,AdminRegistrado)
